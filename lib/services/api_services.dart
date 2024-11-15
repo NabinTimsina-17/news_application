31709ac66a323917295e2d3b3da41c660be29d20
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2024-11-15 11:57:53 +0545

    .

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,10 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
-class HttpService{
+class HttpService {
   late Dio _dio;
 
-  final baseUrl = 'https://newsapi.org/v2/everything?q=tesla&from=2023-12-10&sortBy=publishedAt&apiKey=1ae2573dd0224cfa960a79776d3a21a6';
+  final baseUrl =
+      'https://newsapi.org/v2/everything?q=tesla&from=2024-09-04&sortBy=publishedAt&apiKey=e332b11c23d64f4d93d86aff90eb1442'; // Your API key is included here
 
   HttpService() {
     _dio = Dio(BaseOptions(
@@ -14,21 +15,24 @@ class HttpService{
     initializeInterceptors();
   }
 
-Future<Response> getRequest(String endpoint) async{
+  Future<Response> getRequest(String endpoint) async {
+    try {
+      Response res = await _dio.get(endpoint);
+      return res;
+    } catch (e) {
+      print("Error: $e");
+      rethrow;
+    }
+  }
 
-  Response res;
-
-  res = await _dio.get(endpoint);
-
-  return res;
-
-}
   void initializeInterceptors() {
-    _dio.interceptors.add(PrettyDioLogger( requestHeader: true,
-        requestBody: true,
-        responseBody: true,
-        responseHeader: false,
-        error: true,
-        compact: true,));
+    _dio.interceptors.add(PrettyDioLogger(
+      requestHeader: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: false,
+      error: true,
+      compact: true,
+    ));
   }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,13 +1,8 @@
-// ignore_for_file: avoid_unnecessary_containers
-
 import 'dart:developer';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:news_application/category_card.dart';
 import 'package:news_application/models/news_model.dart';
 import 'package:news_application/services/api_services.dart';
-
 import '../utils/category_list.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,92 +12,140 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-
 class _HomePageState extends State<HomePage> {
-   List<Articles>? articles;
+  List<Articles>? articles;
   late HttpService httpService;
-  void getUserData()async{
-   Response res =await httpService.getRequest("");
-   log(res.toString());
-AllNews allNews = AllNews.fromJson(res.data) ;
-log(allNews.toString());
-articles =allNews.articles;
-setState(() {
-  
-});
-log(articles!.first.title!);
+
+void getUserData() async {
+  try {
+    Response res = await httpService.getRequest("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e332b11c23d64f4d93d86aff90eb1442"); 
+    log(res.toString()); 
+    AllNews allNews = AllNews.fromJson(res.data);
+    log(allNews.toString()); 
+    articles = allNews.articles;
+    log(articles.toString()); 
+    setState(() {});
+  } catch (e) {
+    log("Error: $e");
   }
+}
 
 
   @override
   void initState() {
-    httpService =HttpService();
+    httpService = HttpService();
     getUserData();
     super.initState();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Gandaki",
-              style:
-                  TextStyle(color: Colors.black87, fontWeight: FontWeight.w600),
-            ),
-            Text(
-              "News",
-              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w600),
-            )
-          ],
-        ),
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-      ),
-      body: Column(
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-         ListView.builder(
-          shrinkWrap: true,
-          itemCount: categoryList.length,
-          itemBuilder:(context,index){
-              return  Container(
-                child:Stack(
-                    children: [
-                      Container(
-                      color:Colors.transparent,
-                      child: Text(categoryList[index]["title"]!)
-                      ),
-                      Container(
-                        child: Image.asset(categoryList[index]["url"]!),
-                      )
-                    ],
-                ) );
-          }),
-          Expanded(
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: 100,
-              itemBuilder: (BuildContext context, int index) {
-                
-                
-                return Container(
-                  color: const Color.fromARGB(255, 195, 189, 189),
-                  child: Column(
-                    children: [
-                      Image.network('articles![index].urlToImage'),
-                      Text(articles![index].title!),
-                      Text(articles![index].description!),
-                    ],
-                  ),
-                );
-              },
-            ),
+          Text(
+            "Gandaki",
+            style: TextStyle(color: Colors.black87, fontWeight: FontWeight.w600),
           ),
+          Text(
+            "News",
+            style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w600),
+          )
         ],
       ),
-    );
-  }
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+    ),
+    body: Column(
+      children: [
+        // Category List
+        SizedBox(
+          height: 120,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: categoryList.length,
+            itemBuilder: (context, index) {
+              return Container(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Image.network(
+                      categoryList[index]["url"]!,
+                      height: 80,
+                      width: 100,
+                      fit: BoxFit.cover,
+                    ),
+                    Text(categoryList[index]["title"]!),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+        // Article List
+        Expanded(
+          child: articles == null || articles!.isEmpty
+              ? Center(
+                  child: Text(
+                    'No articles available',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                )
+              : ListView.builder(
+                  itemCount: articles!.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    String? imageUrl = articles?[index].urlToImage;
+                    String? title = articles?[index].title;
+                    String? description = articles?[index].description;
+
+                    return Container(
+                      margin: const EdgeInsets.all(10),
+                      color: const Color.fromARGB(255, 195, 189, 189),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          if (imageUrl != null && imageUrl.isNotEmpty)
+                            Image.network(
+                              imageUrl,
+                              width: double.infinity,
+                              height: 200,
+                              fit: BoxFit.cover,
+                            ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                if (title != null && title.isNotEmpty)
+                                  Text(
+                                    title,
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                if (description != null && description.isNotEmpty)
+                                  Text(
+                                    description,
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.black54,
+                                    ),
+                                  ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+        ),
+      ],
+    ),
+  );
+}
+
 }
